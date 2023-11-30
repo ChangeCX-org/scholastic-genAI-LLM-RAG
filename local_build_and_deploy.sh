af49aa187df5acde327984bad4f6355d1f69acb4
Dasa Ponnappan <dasa@manizh.com>
2023-11-29 21:05:20 -0500

    Checking in changes

--- local_build_and_deploy.sh
+++ local_build_and_deploy.sh
@@ -9,7 +9,7 @@ docker build -t $image_name .
 #Tag the image with $image_name and latest tag
 docker tag $image_name:latest $image_name:latest
 
-#start the container in background
-docker run -d -p 5000:5000 $image_name:latest
+#start the container in background on port 80 (http)
+docker run -d -p 80:80 $image_name:latest
 
 #docker run -p 5000:5000 rag-tokenizer-container:latest
