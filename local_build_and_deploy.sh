e2cc4fce01640fa621bb610c7c5708fe8cc011af
Dasa Ponnappan <dasa@manizh.com>
2023-11-29 21:36:11 -0500

    Checking port

--- local_build_and_deploy.sh
+++ local_build_and_deploy.sh
@@ -10,6 +10,6 @@ docker build -t $image_name .
 docker tag $image_name:latest $image_name:latest
 
 #start the container in background on port 80 (http)
-docker run -d -p 5000:5000 $image_name:latest
+docker run -d -p 5000:5000 --network=host $image_name:latest
 
 #docker run -p 5000:5000 rag-tokenizer-container:latest
