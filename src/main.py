e15eb3e2f223f74f5b3085bdb13ce3657253c82f
Dasa Ponnappan <dasa@manizh.com>
2023-11-29 20:00:57 -0500

    Checing in scripts

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from transformers import RagTokenizer, RagRetriever, RagTokenForGeneration
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 
 class RAGModelHandler:
@@ -12,10 +13,15 @@ class RAGModelHandler:
             "facebook/rag-token-nq", retriever=self.retriever
         )
 
-    def generate_response(self, query, max_length=30):
+    def generate_response(self, query: str, max_length: int = 30):
         input_ids = self.tokenizer(query, return_tensors="pt").input_ids
         output = self.model.generate(input_ids, max_length=max_length)
         return self.tokenizer.decode(output[0], skip_special_tokens=True)
+
+
+class GenerateRequest(BaseModel):
+    query: str
+    max_length: int = 30
 
 
 app = FastAPI()
@@ -23,5 +29,9 @@ model_handler = RAGModelHandler()
 
 
 @app.post("/generate")
-async def generate(query: str, max_length: int = 30):
-    return {"response": model_handler.generate_response(query, max_length)}
+async def generate(request: GenerateRequest):
+    try:
+        response = model_handler.generate_response(request.query, request.max_length)
+        return {"response": response}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
