cf4af36ce64bc3f822e75c5d5e6e4ed20154a2c9
Dasa Ponnappan <dasa@manizh.com>
2023-11-29 19:49:48 -0500

    Checing in scripts

---
+++ local_build_and_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+image_name="rag-tokenizer-container"
+
+
+# Build the Docker image
+docker build -t $image_name .
+
+#Tag the image with $image_name and latest tag
+docker tag $image_name:latest $image_name:latest
+
+#start the container in background
+docker run -d -p 5000:5000 $image_name:latest
